9e653d51ec1522f838d9d77eb029008ebf900a5a
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-23 23:58:51 +0300

    оператор switch goto

---
+++ switch_case/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Калькулятор";
+
+            double a, b;
+            char c;
+
+        L1:
+
+            Console.WriteLine("Введите a");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ведитие знак действия: ");
+            c = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите b");
+            b = double.Parse(Console.ReadLine());
+        
+            switch (c)
+            {
+                case '+':
+
+                    Console.WriteLine("Сумма: " + (a + b));
+
+                    break;
+
+                case '-':
+
+                    Console.WriteLine("Разность: " + (a - b));
+
+                    break;
+                case '*':
+
+                    Console.WriteLine("Произведение: " + (a * b));
+
+                    break;
+                case '/':
+
+                    Console.WriteLine("Частное: " + (a / b));
+
+                    break;
+                default:
+
+                    Console.WriteLine("Ошибка!");
+
+                    break;
+            }
+
+            goto L1;
+
+        }
+    }
+}
