293f409b461fd06b6745a497ea5d1c9b05eac63c
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-23 21:00:35 +0300

    Тернарный оператор

--- Ternarnuy_operator/Program.cs
+++ Ternarnuy_operator/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ternarnuy_operator
+namespace tern_op
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Тернарный оператор";
+
+            int a;
+            bool b;
+
+            Console.WriteLine("Введите число: ");
+            a = int.Parse(Console.ReadLine());
+
+            b = false;
+
+            if (a % 3 == 0)
+            {
+                b = true;
+            }
+
+            Console.WriteLine(b ? (a + " Кратно 3") : (a + " Не кратно 3"));
+
+            Console.ReadKey();
+
         }
     }
 }
