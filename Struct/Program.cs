c407438b808e252612b8c224c4a0e37bbea2d2d9
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-24 19:26:20 +0300

    Structure
    Есть вывод новый

---
+++ Struct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+
+        struct Student
+        {
+            public string name;
+
+            public int date;
+        }
+
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+
+            Console.WriteLine("Введите имя ученика");
+
+            st.name = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+
+            st.date = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Имя {0}\nДата рождения {1}", st.name, st.date);
+
+            Console.ReadKey();
+           
+        }
+    }
+}
