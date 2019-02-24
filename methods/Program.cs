19b098ce6f907bfd2ecc63a8952a2b6fe1fd6253
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-24 22:26:44 +0300

    Methods
    Процедуры и функции

---
+++ methods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        //Процедура
+        public void Add(int int_a, int int_b)
+        {
+            Console.WriteLine(int_a + int_b);
+        }
+
+        static public void Add1( int int_a, int int_b)
+        {
+            Console.WriteLine(int_a + int_b);
+        }
+
+        //Функция
+
+        public int Add2(int int_a, int int_b)
+        {
+            return int_a + int_b;
+        }
+
+        static public int Add3(int int_a, int int_b)
+        {
+            return int_a + int_b;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Введиете a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введиете b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("public void Add");
+            Program p = new Program();
+            p.Add(a, b);
+
+            Console.WriteLine("static public void Add1");
+            Program.Add1(a, b);
+
+            Console.WriteLine("public int Add2 " + p.Add2(a, b));
+
+
+            Console.WriteLine("static public int Add3 " + Add3(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
