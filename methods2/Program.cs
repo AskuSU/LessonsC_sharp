bf1ef288ca21001cca75ec35ed8811009afe2297
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-24 22:46:13 +0300

    methods2
    Функция суммирования цифр четного числа

---
+++ methods2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods2
+{
+    class Program
+    {
+        public static bool Chet(int a)
+        {
+            bool b = true;
+
+            if ((a % 2) != 0)
+            {
+                b = false;
+            }
+
+            return b;
+        }
+
+        public static int SumCifr(int a)
+        {
+            int s = 0;
+
+            while (a != 0)
+            {
+                s += a % 10;
+                a /= 10;
+            }
+
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (Chet(a))
+            {
+                Console.WriteLine(SumCifr(a));
+
+            }
+            else
+            {
+                Console.WriteLine("Нечетное");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
