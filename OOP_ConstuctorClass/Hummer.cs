b16e96cf1978889e4be9eab0c5bcb827cd7c8268
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 22:23:22 +0300

    OOP_ConstructorClass
    Конструкторы класса(несколько вариантов конструктора)

---
+++ OOP_ConstuctorClass/Hummer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ConstuctorClass
+{
+    class Hummer
+    {
+        string firm = null;
+
+        public string Firm
+        {
+            get
+            {
+                return firm;
+            }
+            set
+            {
+                firm = value;
+            }
+        }
+
+        public string material = null;
+
+        public int price;
+
+        public int weight;
+
+        #region Конструкторы
+
+        public Hummer()
+        {
+
+        }
+
+        public Hummer(string firm)
+        {
+            Firm = firm;
+        }
+
+        public Hummer(string firm, string material, int weight)
+        {
+            Firm = firm;
+
+            this.material = material;
+
+            this.weight = weight;
+        }
+
+        public Hummer(int price) : this("Бригадир", "Дерево", 2)
+        {
+            this.price = price;
+   
+        }
+
+        #endregion
+
+    }
+}
---
+++ OOP_ConstuctorClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ConstuctorClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer(2000);
+
+            Console.WriteLine("Фирма {0}, материал {1}, цена {2}, вес {3}", hummer.Firm, hummer.material, hummer.price, hummer.weight);
+
+            Console.ReadKey();
+        }
+    }
+}
