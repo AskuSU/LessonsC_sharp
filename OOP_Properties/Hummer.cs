3480cdf03be384d9d08e5e978a1f4e7523c56aea
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 21:26:56 +0300

    OOP_Properties
    Set  и Get свойства классов

---
+++ OOP_Properties/Hummer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Properties
+{
+    class Hummer
+    {
+        string material = null;
+
+        public string Material
+        {
+            get
+            {
+                return material;
+            }
+            set
+            {
+                if (value.ToLower() != "пластилин")
+                {
+                    material = value;
+                }
+            }
+        }
+    }
+}
---
+++ OOP_Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+
+            string mat = Console.ReadLine();
+
+            hummer.Material = mat;
+
+            Console.WriteLine(hummer.Material);
+
+            Console.ReadKey();
+        }
+    }
+}
