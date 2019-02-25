ed7111aca126329a7e811e6635cb708ce633c38b
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 23:08:43 +0300

    OPP_InitClasses
    Методы использование инициализатора внутри { }
    А так же совместно с конструктором ( )

---
+++ OOP_InitClasses/Hummer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_InitClasses
+{
+    class Hummer
+    {
+        public string firm = null;
+
+        public string Material { get; set; }
+
+        public int Weight { get; set; }
+
+        public int price;
+
+        public Hummer()
+        {
+
+        }
+
+        public Hummer(int price)
+        {
+            this.price = price;
+
+        }
+    }
+}
---
+++ OOP_InitClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_InitClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer(2000)
+            {
+                firm = "Бригадир",
+
+                Material = "Дерево",
+
+                Weight = 2
+            };
+
+            Console.WriteLine("Фирма {0}, материал {1}, вес {2}, цена {3}", hummer.firm, hummer.Material, hummer.Weight, hummer.price);
+
+            Console.ReadKey();
+        }
+    }
+}
