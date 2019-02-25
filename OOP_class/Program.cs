4720c8f89c19ec1692f21991babe10cf5cf50a37
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 17:50:26 +0300

    OPP_class
    Описание классов

---
+++ OOP_class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+
+    //class Hummer
+    //{
+    //    public string material;
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer(); // Доступ по сильной ссылке
+
+            hummer.material = "Дерево";
+
+            Console.WriteLine(hummer.material);
+
+            //new Hummer().material = "Дерево"; // Доступ по слабой ссылке 
+            //Console.WriteLine(new Hummer().material);
+
+            Console.ReadKey();
+        }
+    }
+}
