603668a840a3870fcb0dd8cf493bbaf32938b855
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 21:44:42 +0300

    OOP_RWOPrroperties
    set and get Только для четния или только для записи

---
+++ OOP_RWOProperties/Hummer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RWOProperties
+{
+    class Hummer
+    {
+        string mark = "Бригадир";
+
+        int weigh = 0;
+
+        public string Mark
+        {
+            get
+            {
+                return mark;
+            }
+        }
+
+        public int Weigh
+        {
+            set
+            {
+                if (value != 0)
+                {
+                    weigh = value;
+                }
+            }
+        }
+    }
+}
---
+++ OOP_RWOProperties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RWOProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+
+            string weigh = Console.ReadLine();
+
+            Console.WriteLine(hummer.Mark);
+
+            Console.ReadKey();
+        }
+    }
+}
