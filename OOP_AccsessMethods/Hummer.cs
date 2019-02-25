e35c73c2178d902b8f7b65cc458051d4fefc8057
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 21:06:02 +0300

    OPP_AccsessMethods
    Getter, Setter
    передача и получение недоступных параметров

---
+++ OOP_AccsessMethods/Hummer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_AccsessMethods
+{
+    class Hummer
+    {
+        string material = null;
+
+        public string GetField() //getter, геттер, аксессор
+        {
+            return material;
+        }
+
+        public bool SetField(string material) //setter, сеттер, мутатор
+        {
+            if (material.ToLower() != "пластилин")
+            {
+                this.material = material;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }        
+            
+        }
+
+        public void SetField2(string material)
+        {
+            if (material.ToLower() == "пластилин")
+            {
+                Console.WriteLine(material + " - Недопустимое значение");
+            }
+            else
+            {
+                this.material = material;
+
+                Console.WriteLine(material);
+            }
+        }
+    }
+}
---
+++ OOP_AccsessMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_AccsessMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+
+            string mat = Console.ReadLine();
+
+            if (hummer.SetField(mat))
+            {
+                Console.WriteLine(hummer.GetField());
+            }
+            else
+            {
+                Console.WriteLine(mat + " - недопустимое значение");
+            }
+
+            hummer.SetField2(mat);
+
+            Console.ReadKey();
+        }
+    }
+}
