146f9314cb4744a714e014d1f41fe3d43d495887
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-25 22:33:59 +0300

    OOP_AutoProperties
    Укороченный метод задания свойств конструкторов. {set; get}

---
+++ OOP_AutoProperties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+
+            hummer.Firm = "Бригадир";
+
+            hummer.Material = "Дерево";
+
+            Console.WriteLine("Фирма {0}, материал {1}", hummer.Firm, hummer.Material);
+
+            Console.ReadKey();
+        }
+    }
+}
