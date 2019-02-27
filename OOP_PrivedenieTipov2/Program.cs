e9e34bae20a1311d4b9899ddba6499c33f057cec
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-27 23:38:02 +0300

    OOP_PrivedenieTipov2
    UpCast Показано то что нет создания нового объекта. GetHashCode()

---
+++ OOP_PrivedenieTipov2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_PrivedenieTipov2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            Console.WriteLine(dClass.GetHashCode());
+
+            BaseClass dClassUp = (BaseClass)dClass;
+
+            Console.WriteLine(dClassUp.GetHashCode());
+
+            BaseClass dClassUp1 = new DerivedClass();
+
+            Console.WriteLine(dClassUp1.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
