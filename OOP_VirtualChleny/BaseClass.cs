84b3ca8b2dd6dbeaf4900c664b010d1cc30453e3
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-27 16:57:56 +0300

    OOP_VirtualChleny
    Полиморфизм. Виртуальные члены.
    Переопределение Метода Базового класса

---
+++ OOP_VirtualChleny/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_VirtualChleny
+{
+    class BaseClass
+    {
+        public virtual void Method()
+        {
+            Console.WriteLine("Метод из класса BaseClass");
+        }
+    }
+}
---
+++ OOP_VirtualChleny/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_VirtualChleny
+{
+    class DerivedClass : BaseClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Метод из класса DerivedClass");
+        }
+    }
+}
