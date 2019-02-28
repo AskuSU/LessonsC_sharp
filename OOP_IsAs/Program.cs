350cf7bcff18dbf2e12403357fce3bd8c25d4a1c
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-28 13:44:49 +0300

    OOP_IsAs
    Операторы Is and As. Рассмотрен только Is

---
+++ OOP_IsAs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_IsAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bClass = null;
+
+            DerivedClass dClass = new DerivedClass();
+
+            if (dClass is BaseClass)
+            {
+                bClass = dClass;
+
+                Console.WriteLine("UpCast произведен");
+            }
+            else
+            {
+                bClass = null;
+            }
+            Console.ReadKey();
+        }
+    }
+}
