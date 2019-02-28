565646842b3ab42d2b63e02fa682c2a8a4c53a58
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-02-28 11:42:30 +0300

    OOP_BaseMethod3
    Использование Base без virtual.
    а с помощью new

---
+++ OOP_BaseMethod3/DerivedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_BaseMethod3
+{
+    class DerivedClass : BaseClass
+    {
+        new public void Method(int a, int b)
+        {
+            Console.WriteLine("Переопределенный метод из производного класса DerivedClass ");
+
+            Console.WriteLine(a + b);
+
+            base.Method(a, b);
+        }
+    }
+}
---
+++ OOP_BaseMethod3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_BaseMethod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            dClass.Method(1, 4);
+
+            Console.ReadKey();
+        }
+    }
+}
