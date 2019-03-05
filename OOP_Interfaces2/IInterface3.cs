b7541fba232b5a497d1cb759211057587c6a76c6
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-05 15:19:36 +0300

    OOP_Interfaces2
    Наследование Интерфейсов

---
+++ OOP_Interfaces2/IInterface3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces2
+{
+    interface IInterface3 : IInterface2
+    {
+        void Method3();
+    }
+}
---
+++ OOP_Interfaces2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+
+            dClass.Method1();
+            dClass.Method2();
+            dClass.Method3();
+
+            IInterface2 dClassUp = dClass;
+
+            dClassUp.Method1();
+            dClassUp.Method2();
+
+            Console.ReadKey();
+        }
+    }
+}
