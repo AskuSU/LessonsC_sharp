3c993c643df18dc2d6f51cd8674edce767f17bb3
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-05 12:51:06 +0300

    OOP_Interfaces
    Интерфейсы, метод использования

---
+++ OOP_Interfaces/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces
+{
+    class DerivedClass : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод реализованный в классе DerivedClass");
+        }
+    }
+}
---
+++ OOP_Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            dClass.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
