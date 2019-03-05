e14870756f156805b101854f13a89e8601a0b3ac
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-05 17:13:05 +0300

    OOP_Interfaces5
    Использование интерфейсов с методами одного имени. Так же UpCast вариант.

---
+++ OOP_Interfaces5/DerivedClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces5
+{
+    class DerivedClass : IInterfaceB
+    {
+        void IInterfaceB.Method()
+        {
+            Console.WriteLine("Метод интерфейса А");
+        }
+
+        void IInterfaceA.Method()
+        {
+            Console.WriteLine("Метод интерфейса B");
+        }
+    }
+}
---
+++ OOP_Interfaces5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            IInterfaceA dClassUpToA = dClass as IInterfaceA;
+
+            dClassUpToA.Method();
+
+            Console.WriteLine(dClassUpToA.GetHashCode());
+
+            IInterfaceB dClassUpToB = new DerivedClass();
+
+            dClassUpToB.Method();
+
+            Console.WriteLine(dClassUpToB.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
