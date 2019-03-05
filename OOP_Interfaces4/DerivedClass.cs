d76c7ffd84ab2b5175b5cab38444ff8b7ff00f3b
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-05 17:02:13 +0300

    OOP_Interfaces4
    Использование приватных методов класса наследуемого из интерфейса. Без публичной опубликации методом UpCast.

---
+++ OOP_Interfaces4/DerivedClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces4
+{
+    class DerivedClass : IInterfaceA, IInterfaceB
+    {
+        void IInterfaceA.Method()
+        {
+            Console.WriteLine("Метод интерфейса А");
+        }
+
+        void IInterfaceB.Method()
+        {
+            Console.WriteLine("Метод интерфейса B");
+        }
+    }
+}
---
+++ OOP_Interfaces4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            IInterfaceA dClassUpToA = dClass;
+
+            dClassUpToA.Method();
+
+            IInterfaceB dClassUpToB = dClass as IInterfaceB;
+
+            dClassUpToB.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
