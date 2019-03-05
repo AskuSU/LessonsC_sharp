337c11c833bf95f177dff6faa6e8d04ece05ddd9
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-05 17:24:18 +0300

    OOP_Interfaces6
    Наследование 2х интерфейсов, с одим именем метода, одним классом. Почти как в OOP_Interfaces5. Разница в том что тут техника объединения реализации одноименных абстрактных членов. До этого была техника "Реализация одноименных методов"

---
+++ OOP_Interfaces6/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces6
+{
+    class DerivedClass : IInterfaceA, IInterfaceB
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод интерфейса A и B");
+        }
+    }
+}
---
+++ OOP_Interfaces6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            dClass.Method();
+
+            IInterfaceA dClassUpToA = dClass as IInterfaceA;
+
+            dClassUpToA.Method();
+
+            IInterfaceB dClassUpToB = dClass;
+
+            dClassUpToB.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
