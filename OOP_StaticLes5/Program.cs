0fe426c5d4b9fe4d96816c5a53fae12ae24d3f1e
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-06 18:54:56 +0300

    OOP_StaticLes5
    Статический класс. Переопределение значения статических полей статического класса.

---
+++ OOP_StaticLes5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticClass.field = 30;
+
+            StaticClass.dField = 1.5;
+
+            StaticClass.GetSum();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_StaticLes5/StaticClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes5
+{
+    static class StaticClass
+    {
+        static StaticClass()
+        {
+            field = 10;
+
+            dField = 3.5;
+        }
+
+        public static int field;
+
+        public static double dField;
+
+        public static void GetSum()
+        {
+            Console.WriteLine(field + dField);
+        }
+    }
+}
