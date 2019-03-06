c40b7d539999cac99ef52a672bfa4a586a411735
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-06 14:50:21 +0300

    OOP_Interfaces8
    Наследование от класса и интерфейса, у которых совпатают сигнатуры методов

---
+++ OOP_Interfaces8/IInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces8
+{
+    interface IInterface
+    {
+        string SayHello(string s);
+    }
+}
---
+++ OOP_Interfaces8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interfaces8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+
+            Console.WriteLine(dClass.SayHello("Алексей"));
+
+            Console.ReadKey();
+        }
+    }
+}
