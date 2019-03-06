4aea85e12344c6e13b3042af99663ec1a8f23506
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-06 17:36:07 +0300

    OOP_StaticLes1
    Статика в С#. Ключевое слово Static.
    + Экземплярный метод в классе MyClass

---
+++ OOP_StaticLes1/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes1
+{
+    class MyClass
+    {
+        public int age;
+
+        public int height;
+
+        public static int filed;
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine(filed);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Возраст: {age}\nРост: {height}");
+        }
+    }
+}
---
+++ OOP_StaticLes1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mC1 = new MyClass();
+
+            mC1.age = 17;
+            mC1.height = 180;
+
+            mC1.GetInfo();
+
+            MyClass.filed = 67;
+
+            MyClass.StaticMethod();
+
+            MyClass.filed = 7;
+
+            Console.WriteLine(MyClass.filed);
+
+            Console.ReadKey();
+        }
+    }
+}
