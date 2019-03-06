87e5c910d1fc74ede375db84ef8818b75ff9e554
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-06 17:56:48 +0300

    OOP_StaticLes2
    Вызов статичных конструкторов без их явного вызова.

---
+++ OOP_StaticLes2/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes2
+{
+    class MyClass
+    {
+        public static int filed = 3;
+
+        static MyClass()
+        {
+            //filed = 1;
+            Console.WriteLine("Статический конструктор");
+        }
+
+        public MyClass()
+        {
+            Console.WriteLine("Обычный конструктор");
+        }
+    }
+}
---
+++ OOP_StaticLes2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_StaticLes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(MyClass.filed);
+
+            //MyClass mC = new MyClass();
+
+            MyClass.filed = 6;
+
+            Console.ReadKey();
+        }
+    }
+}
