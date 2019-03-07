cadff30ebb452de5c848f03bee2f2ee4fc7b6803
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-07 13:06:32 +0300

    OOP_ExMethods
    Расширяющие Методы. Расширяют функционал типов среды

---
+++ OOP_ExMethods/ExtensionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ExMethods
+{
+    static class ExtensionClass
+    {
+        public static void SayLOL(this string s)
+        {
+            Console.WriteLine($"{s}, LOL)))))");
+        }
+
+        public static string AddStrings(this string s, params string[] strings)
+        {
+            for (int i=0; i < strings.Length; i++)
+            {
+                s += $" {strings[i]}";
+            }
+
+            return s;
+        }
+    }
+}
---
+++ OOP_ExMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ExMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "Java";
+
+            p.SayLOL();
+
+            ExtensionClass.SayLOL("Python");
+
+            string str = "we love";
+
+            Console.WriteLine(str.AddStrings("C#","Java","C++"));
+
+            string str1 = "I love".AddStrings("c#");
+
+            Console.WriteLine(str1);
+
+            Console.ReadKey();
+        }
+    }
+}
