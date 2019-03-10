8a265ded8baf0dcea3f442ae6bda5727380e1a64
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-10 23:08:33 +0300

    OOP_Boxing_Unboxing
    Упаковка и распаковка типов в тип object

---
+++ OOP_Boxing_Unboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            //boxing
+            object obj = i;
+
+            //unboxing
+            int j = (int)obj;
+
+            Console.WriteLine(j);
+
+            Console.ReadKey();
+
+        }
+    }
+}
