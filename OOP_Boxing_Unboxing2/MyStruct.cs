e2dec728298b422ea61bed5d4a3d307e1a2030c9
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-10 23:24:37 +0300

    OOP_Boxing_Unboxing2
    Упаковка и распаковка моей структуры в тип object, ValueType и в Интерфейс.

---
+++ OOP_Boxing_Unboxing2/MyStruct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Boxing_Unboxing2
+{
+    struct MyStruct : IInterface
+    {
+        public void SayGoodbay()
+        {
+            Console.WriteLine("Goodbay!");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        
+    }
+}
---
+++ OOP_Boxing_Unboxing2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Boxing_Unboxing2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct mS = new MyStruct();
+
+            //Тип 1 (в object)
+            object boxed1 = mS;
+
+            MyStruct unboxed1 = (MyStruct)boxed1;
+
+            unboxed1.SayHello();
+
+            //Тип 2 (в ValueType)
+            ValueType boxed2 = mS;
+
+            MyStruct unboxed2 = (MyStruct)boxed2;
+
+            unboxed2.SayHello();
+
+            //Тип 3 (в интерфейс)
+            IInterface boxed3 = mS;
+
+            MyStruct unboxed3 = (MyStruct)boxed3;
+
+            unboxed3.SayHello();
+
+            unboxed3.SayGoodbay();
+
+            Console.ReadKey();
+
+        }
+    }
+}
