056b33e3887257677572107f98c3469757a83b9d
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-14 23:18:26 +0300

    OOP_Enums2
    Перечисление как счетчик цикла

---
+++ OOP_Enums2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Enums2
+{
+    enum Number
+    {
+        Zero,
+        One,
+        Two,
+        Three
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (Number n = Number.Zero; n <= Number.Three; n++)
+            {
+                Console.Write($"{n}  :  {(int)n}\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
