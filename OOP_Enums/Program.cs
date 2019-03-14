f5b94ddbedac5a1be6ae2c22d70c3078c9bff4b0
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-14 23:07:52 +0300

    OOP_Enums
    Перечисления. Методы использования

---
+++ OOP_Enums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Enums
+{
+    enum Number
+    {
+        Zero,
+        One,
+        Two,
+        Three
+    }
+
+    class Program
+    {
+        private static Number myDigit = Number.One;
+
+        static void Main(string[] args)
+        {
+            Number digit = Number.Two;
+
+            Console.WriteLine(digit);
+
+            Console.WriteLine((int)digit);
+
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine(Number.Three);
+
+            Console.WriteLine((int)Number.Three);
+
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine((int)myDigit);
+
+            Console.ReadKey();
+        }
+    }
+}
