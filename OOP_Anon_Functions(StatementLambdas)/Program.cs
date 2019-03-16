c49d841818bf536bcc95953938962bde2967325e
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-16 21:06:50 +0300

    OOP_Anon_Functions(StatementLambdas + 2)
    Лямбда операторы

---
+++ OOP_Anon_Functions(StatementLambdas)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Anon_Functions_StatementLambdas_
+{
+
+    public delegate void HelloDelegate(string s);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate hD = (x) => { Console.WriteLine("Привет, " + x); };
+
+            hD("Вася");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_Anon_Functions(StatementLambdas2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Anon_Functions_StatementLambdas2_
+{
+
+    public delegate int SubDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SubDelegate sD = (s, d) =>
+            //{
+            //    int res = s - d;
+
+            //    return res;
+            //};
+
+            // int c = sD(10, 8);
+
+            int c = new SubDelegate((s, d) =>
+            {
+                return s - d;
+            }).Invoke(10, 8);
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
