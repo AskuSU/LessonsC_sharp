d887261d27a7b99fde24503c49e37eb2384ab986
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-16 21:32:39 +0300

    OOP_DelegatesArray + 2
    Массив методов(Массив делегатов)

---
+++ OOP_DelegatesArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_DelegatesArray
+{
+
+    public delegate void MyDelegate(string s);
+
+    class Program
+    {
+        private static void SayHello(string str)
+        {
+            Console.WriteLine("Hello, " + str);
+        }
+
+        static void Main(string[] args)
+        {
+            //массив делегатов
+            MyDelegate[] delsArray = new MyDelegate[10];
+
+            for (int i = 0; i< delsArray.Length; i++)
+            {
+                //delsArray[i] = new MyDelegate(SayHello);
+                delsArray[i] = SayHello;
+            }
+
+            for (int i = 0; i < delsArray.Length; i++)
+            {
+                delsArray[i](Convert.ToString(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_DelegatesArray2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_DelegatesArray2
+{
+
+    public delegate void MyDelegate(string s);
+
+    class Program
+    {
+
+        private static void SayHello(string str)
+        {
+            Console.WriteLine("Hello, " + str);
+        }
+
+        private static void SayGoodbye(string str)
+        {
+            Console.WriteLine("Goodbye, " + str);
+        }
+
+        private static void AskHowAreYou(string str)
+        {
+            Console.WriteLine("How Are You, " + str);
+        }
+
+        static void Main(string[] args)
+        {
+            MyDelegate[] methodsArray = new MyDelegate[]
+            {
+                //SayHello, SayGoodbye, AskHowAreYou
+                delegate (string s)
+                {
+                    Console.WriteLine("Hello, " + s);
+                },
+
+                (s) =>
+                {
+                    Console.WriteLine("Goodbye, " + s);
+                },
+
+                (s) => Console.WriteLine("How Are You, " + s)
+        };
+
+            for (int i = 0 ; i < methodsArray.Length; i++)
+            {
+                methodsArray[i](Convert.ToString(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
