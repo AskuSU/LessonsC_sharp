c102009f01cf459612f41887e8365e4c18f62adc
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-16 17:30:12 +0300

    OOP_Delegates3
    Как связать метод с делегатом

---
+++ OOP_Delegates3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Delegates3
+{
+
+    public delegate string HelloDelegate(string name);
+
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Приввет, {s}!";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+
+            HelloDelegate hD = new HelloDelegate(someClass.SayHello);
+
+            Console.WriteLine(hD.Invoke("Вася"));
+
+            Console.WriteLine(hD("Вася"));
+
+            Console.ReadKey();
+        }
+    }
+}
